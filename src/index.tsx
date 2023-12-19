3136d7ca6ac3001227415878696843322e3235ca
shinya-ml <epsonia_ss08@icloud.com>
2023-12-19 17:12:38 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
-import { createServerClient } from "@supabase/ssr";
-import { SupabaseClient } from "@supabase/supabase-js";
-import { Hono, MiddlewareHandler } from "hono";
-import { deleteCookie, getCookie, setCookie } from "hono/cookie";
-
+import { Hono } from "hono";
 const app = new Hono();
 app.get("/", async (c) => {
 	return c.html(<h1>Hogeeeeeeeee</h1>);
 });
-
 export default app;
