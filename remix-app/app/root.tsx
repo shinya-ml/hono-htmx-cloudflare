a8fefb5b9584e921f249b148d3d93cbc627683e3
shinya-ml <epsonia_ss08@icloud.com>
2023-12-20 15:59:23 +0900

    google loginできたぞ

--- remix-app/app/root.tsx
+++ remix-app/app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/cloudflare";
+import type { LinksFunction, LoaderFunction, LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
   Links,
@@ -7,13 +7,23 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { initializeApp } from "firebase/app";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
+
+export const loader: LoaderFunction = async ({context}: LoaderFunctionArgs) => {
+  const config = String(context.env.FIREBASE_CONFIG);
+  return JSON.parse(config);
+}
+
 export default function App() {
+  const firebaseConfig = useLoaderData();
+  initializeApp(firebaseConfig);
   return (
     <html lang="en">
       <head>
--- remix-app/remix.env.d.ts
+++ remix-app/remix.env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/cloudflare" />
 /// <reference types="@cloudflare/workers-types" />
+declare module "@remix-run/server-runtime" {
+    export interface AppLoadContext {
+        env: {
+            FIREBASE_CONFIG: string;
+        }
+    }
+}
