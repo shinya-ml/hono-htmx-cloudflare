e4e89d7f8d91241774d84c8904ea73acaaa41767
shinya-ml <epsonia_ss08@icloud.com>
2023-12-21 17:13:42 +0900

    fix

--- remix-app/app/auth.tsx
+++ remix-app/app/auth.tsx
@@ -1,27 +1,25 @@
-import {User,getAuth, onAuthStateChanged } from 'firebase/auth'
-import { createContext, useContext,useEffect, useState } from 'react'
+import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext<User | undefined>(undefined);
 export function useAuth() {
-    return useContext(AuthContext);
+	return useContext(AuthContext);
 }
 
-export function Auth ({children}) {
-    const [currentUser, setCurrentUser] = useState<User>()
+export function Auth({ children }: { children: React.ReactNode }) {
+	const [currentUser, setCurrentUser] = useState<User>();
 
-    useEffect(() => {
-        onAuthStateChanged(getAuth(), (user) => {
-            if (user) {
-                setCurrentUser(user);
-            } else {
-                setCurrentUser(undefined);
-            }
-        })
-    }, [])
+	useEffect(() => {
+		onAuthStateChanged(getAuth(), (user) => {
+			if (user) {
+				setCurrentUser(user);
+			} else {
+				setCurrentUser(undefined);
+			}
+		});
+	}, []);
 
-    return (
-        <AuthContext.Provider  value={currentUser}>
-            {children}
-        </AuthContext.Provider>
-    );
+	return (
+		<AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>
+	);
 }
--- remix-app/app/routes/_index.tsx
+++ remix-app/app/routes/_index.tsx
@@ -1,36 +1,44 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
-import {GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
-import {useAuth} from "../auth"
+import {
+	GoogleAuthProvider,
+	getAuth,
+	signInWithPopup,
+	signOut,
+} from "firebase/auth";
+import { useAuth } from "../auth";
 
 const handleSignIn = () => {
-  const provider = new GoogleAuthProvider()
-  signInWithPopup(getAuth(), provider)
-}
+	const provider = new GoogleAuthProvider();
+	signInWithPopup(getAuth(), provider);
+};
 
 const handleSignOut = () => {
-  signOut(getAuth())
-}
-
+	signOut(getAuth());
+};
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: "New Remix App" },
+		{ name: "description", content: "Welcome to Remix!" },
+	];
 };
 
 export default function Index() {
-  const user = useAuth();
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      {user ?
-      (
-        <div>
-          <button onClick={handleSignOut}>logout</button>
-        </div>) :
-      (<button onClick={handleSignIn}>login</button>)
-      }
-    </div>
-  )
+	const user = useAuth();
+	return (
+		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+			<h1>Welcome to Remix</h1>
+			{user ? (
+				<div>
+					<button type="button" onClick={handleSignOut}>
+						logout
+					</button>
+				</div>
+			) : (
+				<button type="button" onClick={handleSignIn}>
+					login
+				</button>
+			)}
+		</div>
+	);
 }
