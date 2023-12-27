dbeb5a3f3743dd30cedf4e4cf425466ada5070a4
shinya-ml <epsonia_ss08@icloud.com>
2023-12-27 13:51:12 +0900

    error時は404

--- remix-app/app/routes/articles.$article_id.tsx
+++ remix-app/app/routes/articles.$article_id.tsx
@@ -1,11 +1,16 @@
-import { ClientLoaderFunctionArgs, useLoaderData } from "@remix-run/react";
+import {
+	ClientLoaderFunctionArgs,
+	isRouteErrorResponse,
+	useLoaderData,
+	useRouteError,
+} from "@remix-run/react";
 
 export async function loader({ params }: ClientLoaderFunctionArgs) {
 	const res = await fetch(
 		`http://localhost:8787/articles/${params.article_id}`,
 	);
 	if (!res.ok) {
-		return {};
+		throw new Response(res.body, { status: res.status });
 	}
 	return res.json();
 }
@@ -18,6 +23,27 @@ export default function ArticleDetail() {
 			<div>{loader.article.title}</div>
 			<div>{loader.article.content}</div>
 			<div>{loader.article.author_name}</div>
+		</div>
+	);
+}
+
+export function ErrorBoundary() {
+	const err = useRouteError();
+	if (isRouteErrorResponse(err)) {
+		return (
+			<div>
+				<h1>Oops!</h1>
+				<pre>{err.status}</pre>
+				<pre>{err.data}</pre>
+			</div>
+		);
+	}
+
+	const unknwonErr = JSON.stringify(err);
+	return (
+		<div>
+			<h1>Unknown Error Happened</h1>
+			<pre>{unknwonErr}</pre>
 		</div>
 	);
 }
